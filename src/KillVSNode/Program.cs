2391e2baa747e5ffe7f9a9c21aefcb85ccb4f7eb
Giovanni Bassi <giggio@giggio.net>
2015-08-12 17:42:50 -0300

    Fix bug that would kill all nodes and add super verbose option

--- src/KillVSNode/Program.cs
+++ src/KillVSNode/Program.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Linq;
 using System.Management;
+using static System.Console;
 
 namespace KillVSNode
 {
@@ -9,21 +10,28 @@ namespace KillVSNode
     {
         public void Main(string[] args)
         {
-            Console.TreatControlCAsInput = false;
-            Console.CancelKeyPress += (s, a) => stopped = true;
+            TreatControlCAsInput = false;
+            CancelKeyPress += (s, a) => stopped = true;
             var verbose = args.Contains("--verbose") || args.Contains("-v");
+            var superVerbose = args.Contains("-vv");
+            if (superVerbose) verbose = true;
 
-            if (verbose) Console.WriteLine("Started watching node process under Visual Studio (devenv)");
+            if (verbose) WriteLine("Started watching node process under Visual Studio (devenv)");
             while (true)
             {
                 var nodeProcesses = Process.GetProcessesByName("node");
                 foreach (var nodeProcess in nodeProcesses)
                 {
-                    if (IsChildOfDevenv(nodeProcess))
+                    if (superVerbose) WriteLine($"Found process id {nodeProcess.Id}");
+                    if (IsChildOfDevenv(nodeProcess, superVerbose))
                     {
+                        if (superVerbose) WriteLine($"Found node process id {nodeProcess.Id} which is a child of devenv that is going to die");
                         nodeProcess.Kill();
                         if (verbose)
-                            Console.WriteLine($"Killed node process id {nodeProcess.Id}");
+                            WriteLine($"Killed node process id {nodeProcess.Id}");
+                    } else if (superVerbose)
+                    {
+                        WriteLine($"Node process is not a child of devenv");
                     }
                 }
                 System.Threading.Thread.Sleep(1000);
@@ -34,15 +42,22 @@ namespace KillVSNode
 
         private bool stopped = false;
 
-        private bool IsChildOfDevenv(Process process)
+        private bool IsChildOfDevenv(Process process, bool superVerbose)
         {
             var parentProcess = process;
             do
             {
                 parentProcess = GetProcessParent(parentProcess);
+                if (superVerbose)
+                {
+                    if (parentProcess != null)
+                        WriteLine($"Found parent process '{parentProcess.ProcessName}' id {parentProcess.Id}");
+                    else
+                        WriteLine($"Parent process not found");
+                }
             }
-            while (parentProcess != null && string.Compare(parentProcess.ProcessName, "devenv", StringComparison.InvariantCultureIgnoreCase) > 0);
-            return parentProcess != null;
+            while (parentProcess != null && !string.Equals(parentProcess.ProcessName, "devenv", StringComparison.InvariantCultureIgnoreCase));
+            return parentProcess != null && string.Equals(parentProcess.ProcessName, "devenv", StringComparison.InvariantCultureIgnoreCase);
         }
 
         private Process GetProcessParent(Process process)
